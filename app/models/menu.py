b9671d6569b016904877b8d788167c3cdc3072aa
softcrowdtest036 <softcrowdtest036@gmail.com>
2025-03-21 18:13:13 +0530

    Update menu.py

--- app/models/menu.py
+++ app/models/menu.py
@@ -3,18 +3,18 @@ from datetime import datetime
 from typing import Optional
 
 class MenuModel(BaseModel):
-    id: str = Field(..., alias="_id")  # Convert MongoDB _id to id
+    id: str = Field(..., alias="_id") 
     name: str
     description: str
     category_name: str
     price: float
     parcel_price: Optional[float] = None
-    image_url: Optional[str] = None  # URL to the uploaded image
+    image_url: Optional[str] = None 
     is_available: bool = True
     is_veg: bool = True
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
 
     class Config:
-        populate_by_name = True  # Ensures alias `_id` is respected
+        populate_by_name = True 
         from_attributes = True
